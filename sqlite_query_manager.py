45208ee0005b10dc4227fcab094d2cb4d729d790
Max <162334963+Maxdgf@users.noreply.github.com>
2025-09-12 01:45:44 +0300

    updating execute_query(), added asyncio, added progressbar

--- sqlite_query_manager.py
+++ sqlite_query_manager.py
@@ -6,11 +6,12 @@
 
 #необходимые библиотеки
 import os
+import asyncio
 import sqlite3
 import tkinter as tk
 import pandas as pd
 from tabulate import tabulate
-from tkinter import ttk, filedialog, messagebox, StringVar
+from tkinter import ttk, filedialog, messagebox, StringVar, IntVar
 from async_tkinter_loop import async_handler, async_mainloop
 from tkinter.scrolledtext import ScrolledText
 
@@ -19,12 +20,13 @@ from tkinter.scrolledtext import ScrolledText
 
 #класс приложения
 class SqliteQueryManagerApp(tk.Tk):
+    #===============================================Константы
+    DB_CONNECTION_TIMEOUT: int = 5
+    ASYNC_OPERATIONS_DELAY: float = 0.2
+    #===============================================Константы
+
     def __init__(self):
         super().__init__()
-
-        #===============================================Константы
-        self.DB_CONNECTION_TIMEOUT = 5
-        #===============================================Константы
 
         #=============================================================================================Окно(настройка)
         self.title("Sqlite db query manager")
@@ -34,6 +36,7 @@ class SqliteQueryManagerApp(tk.Tk):
         #============================================================================Переменные состояния
         self.selected_db = StringVar(self)
         self.user_query = StringVar(self)
+        self.query_execution_progress = IntVar(self)
         #============================================================================Переменные состояния
 
         #=============================================================================================UI
@@ -56,7 +59,7 @@ class SqliteQueryManagerApp(tk.Tk):
 
         query_input = ttk.Entry(query_frame, textvariable=self.user_query, foreground="green")
         query_input.pack(side="left", fill="x", expand=True)
-        query_buttton = ttk.Button(query_frame, text="execute", command=async_handler(self.execute_query))
+        query_buttton = ttk.Button(query_frame, text="execute", command=self.execute_query)
         query_buttton.pack(side="left")
 
         query_frame.pack(fill="x")
@@ -71,6 +74,17 @@ class SqliteQueryManagerApp(tk.Tk):
         horizontal_scrollbar.pack(fill="x")
         self.query_result_field.config(xscrollcommand=horizontal_scrollbar.set, bg="black")
         #--------------------------------------------------------------------Поле результата запроса
+
+        self.progressbar_field_frame = ttk.Frame(self)
+
+        self.progressbar = ttk.Progressbar(self.progressbar_field_frame, orient="horizontal", maximum=100, variable=self.query_execution_progress)
+        self.progressbar.pack(side="left", fill="x", expand=True)
+        self.percent_view = ttk.Label(self.progressbar_field_frame, text="0%")
+        self.percent_view.pack(side="left")
+        self.progress_description = ttk.Label(self.progressbar_field_frame, text="executing your query, please wait.")
+        self.progress_description.pack(side="left")
+
+        self.progressbar_field_frame.pack_forget()
 
         #=============================================================================================UI
 
@@ -91,36 +105,55 @@ class SqliteQueryManagerApp(tk.Tk):
             messagebox.showwarning("Warning", "Db file not selected!")
 
     #асинхронная функция для выполнения запроса к бд(не перегружающая главный поток программы)
+    @async_handler
     async def execute_query(self):
+        self.progressbar_field_frame.pack(fill="x")
+
         try:
             self.clear_query_output_field() #очищаем поле для нового результата
 
             if len(self.selected_db.get()) > 0:
-                #подключаемся, создаём курсор
-                connection = sqlite3.connect(self.selected_db.get(), timeout=self.DB_CONNECTION_TIMEOUT)
-                cursor = connection.cursor()
+                if len(self.user_query.get()) > 0:
+                    #подключаемся, создаём курсор
+                    connection = sqlite3.connect(self.selected_db.get(), timeout=self.DB_CONNECTION_TIMEOUT)
+                    cursor = connection.cursor()
 
-                cursor.execute(self.user_query.get()) 
-                connection.commit() #сохраняем изменения
-                
-                result = cursor.fetchall()
-                connection.close() #закрываем подключение
+                    cursor.execute(self.user_query.get()) 
+                    connection.commit() #сохраняем изменения
 
-                df = pd.DataFrame(result)
-                result_table = tabulate(df, headers="keys", tablefmt="grid")
+                    self.query_execution_progress.set(50)
+                    self.percent_view.config(text=f"({self.query_execution_progress.get()})%")
+                    await asyncio.sleep(self.ASYNC_OPERATIONS_DELAY) #асинхронная задержка
+                    
+                    result = cursor.fetchall()
+                    connection.close() #закрываем подключение
 
-                if len(result_table) > 0: #проверяем, не пуст ли результат
-                    self.query_result_field.insert("1.0", result_table)
-                    messagebox.showinfo("Info", f"Your query:\n[{self.user_query.get()}]\ncompleted succesfully! See result now)))")
+                    self.query_execution_progress.set(80)
+                    self.percent_view.config(text=f"({self.query_execution_progress.get()})%")
+                    await asyncio.sleep(self.ASYNC_OPERATIONS_DELAY) #асинхронная задержка
+
+                    df = pd.DataFrame(result)
+                    result_table = tabulate(df, headers="keys", tablefmt="grid")
+
+                    self.query_execution_progress.set(100)
+                    self.percent_view.config(text=f"({self.query_execution_progress.get()})%")
+                    self.progress_description.config(text="done!")
+
+                    if len(result_table) > 0: #проверяем, не пуст ли результат
+                        self.query_result_field.insert("1.0", result_table)
+                        messagebox.showinfo("Info", f"Your query:\n[{self.user_query.get()}]\ncompleted succesfully! See result now)))")
+                    else:
+                        messagebox.showwarning("Warning", f"Your query:\n[{self.user_query.get()}]\ncompleted succesfully, but query result is empty(")
                 else:
-                    messagebox.showwarning("Warning", f"Your query:\n[{self.user_query.get()}]\ncompleted succesfully, but query result is empty(")
+                    messagebox.showerror("Error", "Your query is empty!")
             else:
-                messagebox.showerror("Error", "Please, select db file.")    
+                messagebox.showerror("Error", "Please, select db file.")  
         except Exception as e:
             messagebox.showerror("Error", f"Query execution failed.\nexception:\n{e}")
+
+        self.progressbar_field_frame.pack_forget()  
     #==========================================================================================================Функции
 
 if __name__ == "__main__":
-    sqliteQueryManagerApp = SqliteQueryManagerApp() #инициализируем класс приложения
+    sqliteQueryManagerApp = SqliteQueryManagerApp()
     async_mainloop(sqliteQueryManagerApp) #запуск главного асинхронного цикла приложения(необходимо для работы асинхронных функций)
-
